a092b3740df2e91e10853dd9b4ec550cacec2437
Дягилев Денис Сергеевич <ddyagilev@cinimex.ru>
2021-06-16 11:34:15 +0300

    cord disabled

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./routes/emp')
+const cors = require('cors');
 var app = express()
 
-app.get('/',function(req,res){
+var corsOptions = {
+    origin: "*"
+}
+
+app.options('*', cors(corsOptions))
+app.use(cors(corsOptions));
+
+app.get('/', function (req, res) {
     res.send('hello world')
 })
-app.use('/emp',router)
+app.use('/emp', router)
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
-mongoose.connection.once('open',function(){
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+mongoose.connection.once('open', function () {
     console.log('Database connected Successfully');
-}).on('error',function(err){
+}).on('error', function (err) {
     console.log('Error', err);
 })
 
-app.listen(8000,function(){
+app.listen(8000, function () {
     console.log('Server is Up')
 })
